ddcba03268aa2583a2139ce6aa3005467670cef4
Lucas Souza <ibmstemp@gmail.com>
2019-07-30 12:42:03 -0300

    contact

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
-  devtool: "sourcemap"
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: '!!html-loader!templates/index.html'
+    })
+  ],
+  devtool: 'sourcemap',
+  mode: "development",
+  module: {
+    rules: [{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s?css$/,
+        exclude: /node_modules/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 };
